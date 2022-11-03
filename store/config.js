e4a8fe0d22710d6e2f76b7bccea91702b5536324
Antoine Zimmermann <antoine.zimmermann@epitech.eu>
2022-11-03 19:26:50 +0100

    Update config.js

--- store/config.js
+++ store/config.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
 export const config = {
-    port: 8080,
+    port: process.env.PORT || 8080,
     mongoDbUri: 'mongodb://localhost/flutter-backend',
     mongoHostName: 'localhost',
     privateKey: fs.readFileSync('./store/private.key'),
